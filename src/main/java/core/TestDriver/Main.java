6b2f180b3cced446198c72dc58a5d6b32e2c962b
otorandell <oscar.plk@gmail.com>
2016-05-16 11:49:29 +0200

    Comitasionfinal
    Esto es solo el principio

---
+++ src/main/java/core/TestDriver/Main.java
@@ -0,0 +1,12 @@
+package core.TestDriver;
+
+/**
+ * Created by otorandell on 12/05/2016.
+ */
+public class Main {
+    public static void main (String[] args){
+        TestDriver driver = new TestDriver(args);
+        driver.testStart();
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import core.ConnectionDDBB.ConnectionMSSQL;
+import core.ConnectionDDBB.DDBBInteractions;
+import core.TestDriver.TestDriver;
+
+import java.util.List;
+
+/**
+ * Created by otorandell on 10/02/2016.
+ */
+public class MainTest {
+    public static void main (String[] args){
+        TestDriver driver = new TestDriver(args);
+        driver.testStart();
+
+    }
+}
